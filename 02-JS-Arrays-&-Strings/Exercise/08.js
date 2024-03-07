1333aed284f7d519c9d1df380b2c918fb6af1776
hristoiliev <hristoiliev.1994@gmail.com>
2024-03-07 20:08:20 +0200

    ss

---
+++ 02-JS-Arrays-&-Strings/Exercise/08.js
@@ -0,0 +1,6 @@
+function pascalSplitter(input) {
+    const matches = input.matchAll(/[A-Z][a-z]*/g);
+    const words = Array.from(matches).map(match => match[0]);
+
+    console.log(words.join(', '));
+}
