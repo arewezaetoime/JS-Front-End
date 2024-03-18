547160f7cd36d53785f0d6193264e1815a79d78b
hristoiliev <hristoiliev.1994@gmail.com>
2024-03-18 17:33:52 +0200

    sslabs

--- 04-Objects-and-Classes/Exercise/05.js
+++ 04-Objects-and-Classes/Exercise/05.js
@@ -8,15 +8,33 @@ function solve(data) {
             level: Number(heroLevel),
             items: items,
         }
-        
+
         ourHeroes.push(heroUnit);
     }
 
-    console.log(ourHeroes);
+    ourHeroes.sort((a, b) => a.level - b.level).forEach(hero => console.log(`Hero: ${hero.name}\nlevel => ${hero.level}\nitems => ${hero.items.join(', ')}`));
+
+    // console.log(ourHeroes);
+}
+
+
+function fancySolve(data) {
+    data.map(row => row.split(' / '))
+        .map(([heroName, heroLevel, items]) => ({
+            name: heroName,
+            level: Number(heroLevel),
+            items: items,
+        })).sort((a, b) => a.level - b.level).forEach(hero => console.log(`Hero: ${hero.name}\nlevel => ${hero.level}\nitems => ${hero.items}`))
 }
 
 solve([
     'Isacc / 25 / Apple, GravityGun',
     'Derek / 12 / BarrelVest, DestructionSword',
     'Hes / 1 / Desolator, Sentinel, Antara'
-    ])
+])
+
+fancySolve(([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+]))
---
+++ 04-Objects-and-Classes/Exercise/06-wordTracker.js
@@ -0,0 +1,13 @@
+function wordTrack(words) {
+    const wordsPattern = words.shift().split(' ');
+
+    for (const line of words) {
+
+    }
+}
+
+
+wordTrack([
+    'this sentence',
+    'In', 'this', 'sentence', 'you', 'have', 'to', 'count', 'the', 'occurrences', 'of', 'the', 'words', 'this', 'and', 'sentence', 'because', 'this', 'is', 'your', 'task'
+])
