a1b296ae894b399a831ffde4e00c08e412f54cb8
hristoiliev <hristoiliev.1994@gmail.com>
2024-04-05 17:38:13 +0300

    last

--- 05-DOM-Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ 05-DOM-Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,3 +1,17 @@
 function toggle() {
-    console.log('TODO:...');
+    const extraElement = document.getElementById('extra');
+    const buttonElement = document.querySelector('.head span.button');
+
+    console.log(buttonElement);
+
+    const currentButtonText = buttonElement.textContent
+
+    if (currentButtonText === 'More') {
+        extraElement.style.display = 'block';
+        buttonElement.textContent = 'Less';
+    }
+    else {
+        buttonElement.textContent = 'More';
+        extraElement.style.display = 'none';
+    }
 }
